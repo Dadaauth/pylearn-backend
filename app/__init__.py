b5a595bbc588956c32e26f140389a798d8d8d91c
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-11-23 15:52:54 +0100

    Feat: add CORS functionality to flask application

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from datetime import timedelta
 from flask import Flask
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 def create_app(testing=False):
     app = Flask(__name__)
+    CORS(app)
     load_dotenv()
     os.environ["TESTING"] = str(testing)
 

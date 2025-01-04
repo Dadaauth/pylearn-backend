da26088eff0af1a0a4450dcc30e13b93f111a5d4
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-01-04 10:49:16 +0100

    Feat: add status atribute to ORM Mapped class Module

--- app/models/module.py
+++ app/models/module.py
@@ -1,5 +1,6 @@
 from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.dialects.mysql import ENUM
 
 from app.models.base import Base
 from app.models.basemodel import BaseModel
@@ -11,6 +12,7 @@ class Module(BaseModel, Base):
 
     title = mapped_column(String(60), nullable=False)
     description = mapped_column(String(300))
+    status = mapped_column(ENUM("deleted", "draft", "published"), default="draft", nullable=False)
     
     projects = relationship("Project", back_populates="module")
 
@@ -20,7 +22,7 @@ class Module(BaseModel, Base):
         super().__init__()
         [setattr(self, key, value) for key, value in kwargs.items()]
 
-        required_keys = {"title"}
+        required_keys = {"title", "status"}
         accurate, missing = has_required_keys(kwargs, required_keys)
         if not accurate:
             raise ValueError(f"Missing required key(s): {', '.join(missing)}")

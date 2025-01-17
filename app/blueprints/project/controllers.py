13f8f2e95dd16988e1b31fd2d73e839dc502ce7e
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-01-17 13:14:28 +0100

    refactor: remove unused modules endpoint from the project blueprint

--- app/blueprints/project/controllers.py
+++ app/blueprints/project/controllers.py
@@ -5,25 +5,6 @@ from app.blueprints.project.services import update_single_project_details, mark_
 from .services import icreate_module, ifetch_modules, iupdate_module
 from app.utils.helpers import format_json_responses, handle_endpoint_exceptions, admin_required
 
-@jwt_required()
-@admin_required
-@handle_endpoint_exceptions
-def create_module():
-    icreate_module()
-    return format_json_responses(201, message="Resource created successfully!")
-
-@jwt_required()
-@admin_required
-@handle_endpoint_exceptions
-def update_module(module_id):
-    iupdate_module(module_id)
-    return format_json_responses(message="Record updated successfully!")
-
-@jwt_required()
-@handle_endpoint_exceptions
-def fetch_modules():
-    modules = ifetch_modules()
-    return format_json_responses(data={"modules": modules})
 
 @jwt_required()
 @handle_endpoint_exceptions

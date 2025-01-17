dc43e2c3174f802829d3066f39dabc76d70400b4
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-01-17 09:44:56 +0100

    feat: Upgrade update method to sort project if its order in the linked list is beign updated

--- app/models/project.py
+++ app/models/project.py
@@ -1,6 +1,7 @@
+from hmac import new
 from uuid import uuid4
 
-from sqlalchemy import DateTime, Integer, String, ForeignKey, Text
+from sqlalchemy import DateTime, Integer, String, ForeignKey, Text, UniqueConstraint
 from sqlalchemy.dialects.mysql import LONGTEXT, ENUM
 from sqlalchemy.orm import mapped_column, relationship
 
@@ -41,8 +42,8 @@ class Project(BaseModel, Base):
             raise ValueError(f"Missing required key(s): {', '.join(missing)}")
         
         # insert Project at the correct node
-        if Project.count() == 0:
-            # insert as head of the list
+        if Project.search(module_id=module_id) is None:
+            # list is empty therefore insert as head of the list
             self.prev_project_id = None
             self.next_project_id = None
             return
@@ -51,15 +52,16 @@ class Project(BaseModel, Base):
         self.refresh()
 
         prev_project_id = kwargs.get("prev_project_id")
+        module_id = kwargs.get("module_id")
         if not prev_project_id:
             # Make first project in list
-            head_project = Project.search(prev_project_id=None)
+            head_project = Project.search(prev_project_id=None, module_id=module_id)
             head_project.prev_project_id = self.id
-            head_project.save() # Add project to session for saving
+            head_project.save()
 
             self.next_project_id = head_project.id
         else:
-            prev_project = Project.search(id=prev_project_id)
+            prev_project = Project.search(id=prev_project_id, module_id=module_id)
             if prev_project is None:
                 raise NotFound("Previous Project Not Found")
             next_p_id = prev_project.next_project_id
@@ -93,6 +95,48 @@ class Project(BaseModel, Base):
         projects = super().search(**filters)
         return cls.sort_projects(projects)
     
+    def update(self, **kwargs: dict) -> None:
+        """
+            You need to intercept here to account for
+            updates involving the arrangement of nodes
+            in the linked list
+
+            Update a set of attributes in an object.
+            :params
+                @kwargs: a dictionary of attributes
+                        to update
+        """
+        if kwargs.get("prev_project_id") is not None:
+            """
+                Code for if the client is trying
+                to update the order of the projects
+                in the linked list.
+            """
+            prev_project_id = kwargs.get("prev_project_id")
+            new_prev_project = Project.search(id=prev_project_id)
+
+            if not new_prev_project:
+                raise NotFound("Previous Project not found")
+
+            if self.prev_project_id == new_prev_project.id:
+                del kwargs["prev_project_id"]
+                super().update(**kwargs)
+                return
+
+            new_prev_project.prev_project_id = self.prev_project_id
+            self.next_project_id = new_prev_project.next_project_id
+
+            if new_prev_project.next_project_id:
+                new_prev_project.next_project.prev_project_id = self.id
+                # add to SQLAlchemy session
+                new_prev_project.next_project.add()
+            self.prev_project_id = new_prev_project.id
+            new_prev_project.next_project_id = self.id
+            # add to SQLAlchemy session
+            new_prev_project.add()
+
+        super().update(**kwargs)
+
     def sort_projects(projects):
         if projects is None or projects == []: return projects
         if not isinstance(projects, list): return projects
@@ -103,7 +147,7 @@ class Project(BaseModel, Base):
             if project.prev_project_id is None:
                 head = project
                 break
-        
+
         # sorting process
         tmp = head
         tmp_list = [head]
@@ -120,12 +164,16 @@ class Project(BaseModel, Base):
 
 class StudentProject(BaseModel, Base):
     __tablename__ = "student_projects"
+    __table_args__ = (
+        UniqueConstraint('student_id', 'project_id', name='unique_student_id__project_id'),
+    )
 
     student_id = mapped_column(ForeignKey("students.id"), nullable=False)
     project_id = mapped_column(ForeignKey("projects.id"), nullable=False)
     status = mapped_column(ENUM("released", "submitted", "graded", "verified"), default="released", nullable=False)
     submission_file = mapped_column(String(300))
     submitted_on = mapped_column(DateTime)
+    assigned_to = mapped_column(ForeignKey("admins.id"))
     graded_on = mapped_column(DateTime)
     graded_by = mapped_column(ForeignKey("admins.id"))
     grade = mapped_column(Integer)

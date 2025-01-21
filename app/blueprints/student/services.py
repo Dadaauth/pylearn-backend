84984a0511cfe47922de296f9a310fd505c1c46f
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-01-21 17:06:47 +0100

    feat: change account activaton requirements from registration_number to email address

--- app/blueprints/student/services.py
+++ app/blueprints/student/services.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from flask_jwt_extended import get_jwt_identity
 from app.utils.helpers import extract_request_data
-from app.utils.error_extensions import BadRequest, NotFound
+from app.utils.error_extensions import BadRequest, InternalServerError, NotFound
 from app.models.user import Student, Admin
 from app.models.module import Module
 from app.models.project import Project, StudentProject
@@ -143,13 +143,16 @@ def jfetch_projects(module_id):
 
 def activate_student_account():
     data = extract_request_data("json")
-    registration_number = data.get("registration_number")
-    if not registration_number:
-        raise BadRequest("Missing required field: registration_number")
+    email = data.get("email")
+    if not email:
+        raise BadRequest("Missing required field: email")
 
-    student = Student.search(registration_number=registration_number)
+    student = Student.search(email=email)
     if not student:
-        raise NotFound(f"Student with Registration Number {registration_number} not found")
+        raise NotFound(f"Student with Email {email} not found")
+    
+    if isinstance(student, list):
+        raise InternalServerError("Multiple students with same email address")
     
     if not (data.get("username") and data.get("password") and data.get("phone")):
         raise BadRequest("Missing required field(s): username, password, phone")

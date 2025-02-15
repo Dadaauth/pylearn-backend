1f6ce715ec2dada038be5616fd3a8b1203690248
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-02-15 08:45:59 +0100

    feat: add mentors blueprint

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -2,6 +2,7 @@ from app.blueprints.auth import auth_bp
 from app.blueprints.module import module_bp
 from app.blueprints.project import project_bp
 from app.blueprints.admin import admin_bp
+from app.blueprints.mentor import mentor_bp
 from app.blueprints.student import student_bp
 
 def register_blueprints(app):
@@ -9,4 +10,5 @@ def register_blueprints(app):
     app.register_blueprint(module_bp, url_prefix='/api/v1/module')
     app.register_blueprint(project_bp, url_prefix='/api/v1/project')
     app.register_blueprint(admin_bp, url_prefix='/api/v1/admin')
+    app.register_blueprint(mentor_bp, url_prefix='/api/v1/mentor')
     app.register_blueprint(student_bp, url_prefix='/api/v1/student')
---
+++ app/blueprints/mentor/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+
+mentor_bp = Blueprint('mentor', __name__)
+
+# mentor_bp.add_url_rule("/", view_func=, methods=["POST"])

0fb8e1f4f3aa53d31d5ba00edec419fbcec6a2c9
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-02-15 19:47:42 +0100

    feat: change '/student/students' GET endpoint to '/student/all'

--- app/blueprints/v1/student/__init__.py
+++ app/blueprints/v1/student/__init__.py
@@ -6,7 +6,7 @@ student_bp = Blueprint('student', __name__)
 
 # Admin and Mentor access only functionalities
 student_bp.add_url_rule("/create", view_func=create_new_student, methods=["POST"])
-student_bp.add_url_rule("/students", view_func=all_students, methods=["GET"])
+student_bp.add_url_rule("/all", view_func=all_students, methods=["GET"])
 
 # Student Functionalities
 student_bp.add_url_rule('/account/activate', view_func=activate_account, methods=['POST'])

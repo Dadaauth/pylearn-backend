e4f39bf12eff30d24a15c64bf32f2f66e95602a0
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-02-15 20:17:55 +0100

    feat: move functionality to grade project and retrieve projects with submissions from 'admin' to 'project' blueprint

--- app/blueprints/v1/admin/controllers.py
+++ app/blueprints/v1/admin/controllers.py
@@ -1,18 +1,2 @@
 from flask_jwt_extended import jwt_required
 from app.utils.helpers import admin_required, format_json_responses, handle_endpoint_exceptions
-from .services import igrade_student_project
-from .services import iretrieve_projects_with_submissions
-
-@jwt_required()
-@admin_required
-@handle_endpoint_exceptions
-def grade_student_project():
-    igrade_student_project()
-    return format_json_responses(message="Grading successful")
-
-@jwt_required()
-@admin_required
-@handle_endpoint_exceptions
-def retrieve_projects_with_submissions():
-    projects = iretrieve_projects_with_submissions()
-    return format_json_responses(data={"projects": projects})
--- app/blueprints/v1/project/__init__.py
+++ app/blueprints/v1/project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from app.blueprints.v1.project.controllers import create_project, fetch_projects_for_module, fetch_single, update_single
 from app.blueprints.v1.project.controllers import generate_project_submission, retrieve_assigned_project_submissions
+from app.blueprints.v1.project.controllers import retrieve_projects_with_submissions, grade_student_project
 
 project_bp = Blueprint('project', __name__)
 
@@ -12,6 +13,8 @@ project_bp.add_url_rule('/all', view_func=fetch_projects_for_module, methods=['G
 project_bp.add_url_rule('/<project_id>', view_func=fetch_single, methods=['GET'])
 project_bp.add_url_rule("/<project_id>/submissions/generate", view_func=generate_project_submission, methods=["GET"])
 project_bp.add_url_rule("/<project_id>/assigned_submissions", view_func=retrieve_assigned_project_submissions, methods=["GET"])
+project_bp.add_url_rule("/projects/with_submissions", view_func=retrieve_projects_with_submissions, methods=["GET"])
+project_bp.add_url_rule("/project/grade", view_func=grade_student_project, methods=["PATCH"])
 
 project_bp.add_url_rule('/edit/<project_id>', view_func=update_single, methods=['PATCH'])
 project_bp.add_url_rule('/create', view_func=create_project, methods=['POST'])

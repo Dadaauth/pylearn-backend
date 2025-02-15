bd10d2934ccc7d9ec2b534ad4115eb46021cb793
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-02-15 20:19:58 +0100

    refactor: remove unnecessary line

--- app/blueprints/v1/project/__init__.py
+++ app/blueprints/v1/project/__init__.py
@@ -8,13 +8,11 @@ project_bp = Blueprint('project', __name__)
 
 project_bp.add_url_rule('/all', view_func=fetch_projects_for_module, methods=['GET'])
 
-
 # Admin and Mentor access only functionalities
 project_bp.add_url_rule('/<project_id>', view_func=fetch_single, methods=['GET'])
 project_bp.add_url_rule("/<project_id>/submissions/generate", view_func=generate_project_submission, methods=["GET"])
 project_bp.add_url_rule("/<project_id>/assigned_submissions", view_func=retrieve_assigned_project_submissions, methods=["GET"])
 project_bp.add_url_rule("/projects/with_submissions", view_func=retrieve_projects_with_submissions, methods=["GET"])
 project_bp.add_url_rule("/project/grade", view_func=grade_student_project, methods=["PATCH"])
-
 project_bp.add_url_rule('/edit/<project_id>', view_func=update_single, methods=['PATCH'])
 project_bp.add_url_rule('/create', view_func=create_project, methods=['POST'])

87f5e0cc23a5e71d96e3327a437c0af7d1418ec2
Dada Clement Authority <dadaauthourity23@gmail.com>
2025-02-25 11:27:35 +0100

    feat: add protection to mentor specific route

--- app/blueprints/v1/mentor/controllers.py
+++ app/blueprints/v1/mentor/controllers.py
@@ -1,6 +1,6 @@
 from flask_jwt_extended import jwt_required
 
-from app.utils.helpers import admin_required, format_json_responses, handle_endpoint_exceptions, format_json_responses
+from app.utils.helpers import admin_required, mentor_required, format_json_responses, handle_endpoint_exceptions, format_json_responses
 from .services import all_mentors_data, activate_mentor_account, imentor_assigned_cohorts, imentor_assigned_cohorts_for_admin
 
 @jwt_required()
@@ -16,6 +16,7 @@ def activate_account():
     return format_json_responses(200, message="Account activated successfully.")
 
 @jwt_required()
+@mentor_required
 @handle_endpoint_exceptions
 def mentor_assigned_cohorts():
     cohorts = imentor_assigned_cohorts()
